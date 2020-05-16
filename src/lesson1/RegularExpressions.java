3fa1c19ce9e9baba979204621ce50113a326f4e5
taka1127 <taka07251127@gmail.com>
2020-05-16 22:29:01 +0900

    20200516

--- src/lesson1/RegularExpressions.java
+++ src/lesson1/RegularExpressions.java
@@ -11,7 +11,17 @@ public class RegularExpressions {
 		System.out.println(s.matches("J.va"));
 		System.out.println("Jaaaaaava".matches("Ja*va"));
 		System.out.println("あいうxx019".matches(".*"));
-		
+		System.out.println("url".matches("[a-z]{3}"));
+
+		String s2 = "abc,def:ghi";
+		String[] words = s2.split("[,:]");
+		for(String w : words) {
+			System.out.print(w + "->");
+		}
+		String s3 = "abc,def:ghi";
+		String w = s3.replaceAll("[bef]","X");
+		System.out.println("\n"+w);
+
 
 
 	}
